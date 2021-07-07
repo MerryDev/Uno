4fd34ea0fb7dc875ef3c16488376392ac6ddfa2d
Merry <svgr1808@outlook.de>
2021-07-07 22:02:43 +0200

    Added documentation

--- src/main/java/de/digitaldevs/uno/utils/ElementWrapper.java
+++ src/main/java/de/digitaldevs/uno/utils/ElementWrapper.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
+ * This class represents a generic wrapper for elements wich should be linked with another wrapped element.
+ *
  * @author Sven
  * @author <a href='https://digitaldevs.de'>DigitalDevs.de</a>
  * @version 1.0.0
