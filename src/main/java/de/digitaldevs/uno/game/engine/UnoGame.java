a0626dc34f7d817d95ad252b9d97763398c02188
Merry <svgr1808@outlook.de>
2021-07-08 15:28:18 +0200

    Created provisional constructor & fields

--- src/main/java/de/digitaldevs/uno/game/engine/UnoGame.java
+++ src/main/java/de/digitaldevs/uno/game/engine/UnoGame.java
@@ -1,9 +1,28 @@
 package de.digitaldevs.uno.game.engine;
 
+import de.digitaldevs.uno.game.engine.cards.Card;
+import de.digitaldevs.uno.utils.LinkedQueue;
+import de.digitaldevs.uno.utils.Stack;
+
 /**
  * @author Sven
  * @author <a href='https://digitaldevs.de'>DigitalDevs.de</a>
  * @version 1.0.0
  */
 public class UnoGame {
+
+    private LinkedQueue<Player> players;
+    private Stack<Card> hiddenCards;
+    private Stack<Card> openCards;
+
+    private Card startCard;
+
+    public UnoGame() {
+        this.hiddenCards = new Stack<>();
+        this.openCards = new Stack<>();
+        this.startSetup();
+    }
+
+    private void startSetup() { }
+
 }
